e95a0b0f5ab1cda5b19e9e1d1d8c3243c8b5fc21
stone-rose32 <150394141+stone-rose32@users.noreply.github.com>
2024-04-29 12:56:30 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,15 @@ import streamlit as st
 from pages import introduction, about, data_and_visuals
 
 def main():
+    st.sidebar.title("Navigation")
+
+    # Buttons for each page
+    if st.sidebar.button("Introduction"):
+        introduction.show()
+    if st.sidebar.button("About"):
+        about.show()
+    if st.sidebar.button("Data and Visuals"):
+        data_and_visuals.show()
 
 if __name__ == "__main__":
     main()
