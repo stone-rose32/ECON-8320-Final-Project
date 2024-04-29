4148aee6faba9ff5b98fb4d9ce42392ba2faf6c0
stone-rose32 <150394141+stone-rose32@users.noreply.github.com>
2024-04-29 10:58:55 -0500

    Added paging

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,1 +1,16 @@
+import streamlit as st
+from pages import introduction, about, data_and_visuals
 
+def main():
+    st.sidebar.title("Navigation")
+    page = st.sidebar.selectbox("Choose a page", ["Introduction", "About", "Data and Visuals"])
+
+    if page == "Introduction":
+        introduction.show()
+    elif page == "About":
+        about.show()
+    elif page == "Data and Visuals":
+        data_and_visuals.show()
+
+if __name__ == "__main__":
+    main()
