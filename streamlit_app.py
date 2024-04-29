95d2363e62cf74a451f58822bf3c36c8dc57307d
stone-rose32 <150394141+stone-rose32@users.noreply.github.com>
2024-04-29 12:50:28 -0500

    Update streamlit_app.py Sidebar

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,19 +2,6 @@ import streamlit as st
 from pages import introduction, about, data_and_visuals
 
 def main():
-    st.set_page_config(
-        page_title="Hello",
-        page_icon="👋",
-    )
-    st.sidebar.title("Navigation")
-    page = st.sidebar.selectbox("Choose a page", ["Introduction", "About", "Data and Visuals"])
-
-    if page == "Introduction":
-        introduction.show()
-    elif page == "About":
-        about.show()
-    elif page == "Data and Visuals":
-        data_and_visuals.show()
 
 if __name__ == "__main__":
     main()
