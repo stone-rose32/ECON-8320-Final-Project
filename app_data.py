7b089e6187796a4da11bb5a4064e475f44f77fe6
stone-rose32 <150394141+stone-rose32@users.noreply.github.com>
2024-05-01 15:25:31 -0500

    Update app_data.py
    Made variable an attribute

--- app_data.py
+++ app_data.py
@@ -1,50 +1,55 @@
 import requests
 import json
 import pandas as pd
+from tqdm import tqdm
 
-def call_url(year, month):
+def call_url(year, month, variables):
+    url_var = variables[0]
+    for var in variables[1:]:
+      url_var = str(url_var + ',' + var)
     base_url = 'https://api.census.gov/data/'
     year_month = f'{year}/cps/basic/{month}'
-    census_var = 'get=GTCBSA,HEHOUSUT,HETENURE,HEFAMINC,PEEDUCA,PEMLR,PESEX,PEMARITL'
+    census_var = f'get={url_var}'
     api_key = '9f3223244e7252dcbb1ed8348a071fc3673eb56e'
-    
+
     url = f'{base_url}{year_month}?{census_var}&key={api_key}'
-    
+
     return url
 
 def get_data(url):
     response = requests.get(url)
     return response.json()
 
-def get_data_from_census(years, months):
+def get_data_from_census(years, months, variables):
     all_data = []
-    for year in years:
+    urls = []
+    for year in tqdm (years, desc="Loading..."):
         for month in months:
-            url = call_url(year, month)
+            url = call_url(year, month, variables)
+            urls.append(url)
             data = get_data(url)
             yearcol = 'Year'
             monthcol = 'Month'
             data[0].append(yearcol)
             data[0].append(monthcol)
             for row in data[1:]:
-                row.append(year)  # Add year to each row
-                row.append(month)  # Add month to each row
+                row.append(year)
+                row.append(month)
             all_data.extend(data)
-            print(year+'-'+month)
-    return all_data
+    columns = all_data[0]
+    rows = all_data[1:]
+
+    census_data = pd.DataFrame(rows, columns=columns)
+    return census_data, urls
 
 def main():
-    years = ['2010', '2011', '2012', '2013', '2014', '2015', '2016', '2017', '2018', '2019', '2020', '2021', '2022', '2023']
+    years = [2010, 2011, 2012, 2013, 2014]
     months = ['jan']
-    data = get_data_from_census(years, months)
-    columns = data[0]
-    rows = data[1:]
-    
-    df = pd.DataFrame(rows, columns=columns)
-    
-    df.to_csv('census_data.csv', index=False)
+    variables = ['GTCBSA', 'HEHOUSUT', 'HETENURE', 'HEFAMINC', 'PEEDUCA', 'PEMLR', 'PESEX', 'PEMARITL']
+    data, urls = get_data_from_census(years, months, variables)
+
+    data.to_csv('census_data.csv', index=False)
 
 
 if __name__ == "__main__":
     main()
-
