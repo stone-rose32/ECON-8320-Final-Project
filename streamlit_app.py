7e115ca6ae7748f5c0495644d235860c19998ca9
stone-rose32 <150394141+stone-rose32@users.noreply.github.com>
2024-05-04 17:34:50 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,5 @@
 import streamlit as st
-from pages import project_details, about, data_and_visuals
+from pages import project_details, about, data_and_visuals, project_code
 
 def show():
     st.title("Introduction")
