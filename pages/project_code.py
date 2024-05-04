29ab2d34273e30301e5d82bf8033399bec6b9cd4
stone-rose32 <150394141+stone-rose32@users.noreply.github.com>
2024-05-04 17:32:52 -0500

    Create project_code.py

---
+++ pages/project_code.py
@@ -0,0 +1,56 @@
+import streamlit as st
+import requests
+
+def get_data(url):
+  response = requests.get(url)
+  return response.text
+
+def main():
+    st.title("Project Code")
+    pc_intro_text = """
+    This tab's purpose is to show the code used in this project. Ultimately, there are aspects
+      of the project's code I know can be improved upon which I touched on in the "About" page. 
+      Alas, at some point, the project meets the required criteria, and it becomes time to 
+      move on. As Leonadro Da Vinci said, "Art is never finished, only abandoned."
+
+    Welp, consider this abandoned for the time being. The source code can be found
+      on the Github repository linked below. Feel free to suggest any edits!
+
+    [Link to Github Repository](https://github.com/stone-rose32/ECON-8320-Final-Project/tree/main)
+    """
+    st.write(pc_intro_text)
+    
+    tab1, tab2, tab3, tab4, tab5, tab6 = st.tabs(["Initial Data Extraction", "Streamlit Application Core", "About Page", "Data and Visual Page", "Project Details Page", "Project Code Page"])
+    with tab1:
+        app_data_url = 'https://raw.githubusercontent.com/stone-rose32/ECON-8320-Final-Project/main/app_data.py'
+        data_code = get_data(app_data_url)
+        st.header('Initial Data Extraction Code')
+        st.code(data_code, language='python')
+    with tab2:
+        app_data_url = 'https://raw.githubusercontent.com/stone-rose32/ECON-8320-Final-Project/main/streamlit_app.py'
+        data_code = get_data(app_data_url)
+        st.header('Streamlit Application Core Code')
+        st.code(data_code, language='python')
+    with tab3:
+        app_data_url = 'https://raw.githubusercontent.com/stone-rose32/ECON-8320-Final-Project/main/pages/about.py'
+        data_code = get_data(app_data_url)
+        st.header('About Page Code')
+        st.code(data_code, language='python')
+    with tab4:
+        app_data_url = 'https://raw.githubusercontent.com/stone-rose32/ECON-8320-Final-Project/main/pages/data_and_visuals.py'
+        data_code = get_data(app_data_url)
+        st.header('Introdution Page Code')
+        st.code(data_code, language='python')
+    with tab5:
+        app_data_url = 'https://raw.githubusercontent.com/stone-rose32/ECON-8320-Final-Project/main/pages/project_details.py'
+        data_code = get_data(app_data_url)
+        st.header('Data and Visual Page Code')
+        st.code(data_code, language='python')
+    with tab6:
+        app_data_url = 'https://raw.githubusercontent.com/stone-rose32/ECON-8320-Final-Project/main/pages/project_code.py'
+        data_code = get_data(app_data_url)
+        st.header('Project Code Page Code')
+        st.code(data_code, language='python')
+
+if __name__ == "__main__":
+    main()
