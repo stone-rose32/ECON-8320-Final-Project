14fbbdb9aa0652a11e3fc4937c8963ebff6a837c
Austin R <150394141+stone-rose32@users.noreply.github.com>
2024-05-05 12:54:12 -0500

    Update 3_data_and_visuals.py

--- pages/3_data_and_visuals.py
+++ pages/3_data_and_visuals.py
@@ -1,8 +1,12 @@
 import streamlit as st
+import pandas as pd
 
 def show():
     st.title("Data and Visuals")
-    st.write("Explore data and visualizations in this section.")
+    
+    census_data = pd.read_csv("https://raw.githubusercontent.com/stone-rose32/ECON-8320-Final-Project/main/census_data.csv")
+
+    st.dataframe(census_data)
 
 def main():
     show()
