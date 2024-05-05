06d9ad2a3458308f9bf94ef5aad165db67d01453
Austin R <150394141+stone-rose32@users.noreply.github.com>
2024-05-05 13:14:17 -0500

    Update 3_data_and_visuals.py

--- pages/3_data_and_visuals.py
+++ pages/3_data_and_visuals.py
@@ -1,15 +1,18 @@
 import streamlit as st
 import pandas as pd
 
-def show():
+def show(data):
     st.title("Data and Visuals")
     
-    census_data = pd.read_csv("https://raw.githubusercontent.com/stone-rose32/ECON-8320-Final-Project/main/census_data.csv")
-
-    st.dataframe(census_data)
+    tab1, tab2 = st.tabs(["Visualiztions", "Data Table"])
+    with tab1:
+        st.line_chart(data, x = "Year", y = ["HEHOUSUT", "HETENURE", "HEFAMINC"])
+    with tab2:    
+        st.dataframe(data)
 
 def main():
-    show()
+    census_data = pd.read_csv("https://raw.githubusercontent.com/stone-rose32/ECON-8320-Final-Project/main/census_data.csv")
+    show(census_data)
 
 
 if __name__ == "__main__":
