333e78bc6348f8679254565ffa27d703b445b353
Austin R <150394141+stone-rose32@users.noreply.github.com>
2024-05-06 21:44:17 -0500

    Update 3_data_and_visuals.py

--- pages/3_data_and_visuals.py
+++ pages/3_data_and_visuals.py
@@ -6,7 +6,22 @@ def show(data):
     
     tab1, tab2 = st.tabs(["Visualiztions", "Data Table"])
     with tab1:
-        st.line_chart(data, x = "Year", y = ["Household-type of living quarters", "Household-own/rent living quarters", "Household-total family income in past 12 months"])
+        st.title('CPS Data Analysis')
+    
+        selected_variable = st.selectbox('Select a variable', data.columns)
+    
+        if selected_variable == 'Household-total family income in past 12 months':
+            fig = px.histogram(data, x=selected_variable, title='Distribution of Household Total Family Income')
+        elif selected_variable == 'Household-type of living quarters':
+            fig = px.pie(data, names=selected_variable, title='Percentage of Household Types of Living Quarters')
+        elif selected_variable == 'Demographics-sex':
+            fig = px.bar(data['Demographics-sex'].value_counts().reset_index(), x='index', y='Demographics-sex', title='Count of Males and Females')
+        elif selected_variable == 'Household-total # of members':
+            fig = px.box(data, y=selected_variable, title='Distribution of Household Total Number of Members')
+        elif selected_variable == 'Month':
+            fig = px.line(data.groupby(selected_variable).size().reset_index(), x=selected_variable, y=0, title='Trends Over Time')
+    
+        st.plotly_chart(fig)
     with tab2:    
         st.dataframe(data)
 
