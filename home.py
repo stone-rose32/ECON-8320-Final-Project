727aefc5e7b39aa1ae0ae7afdc80f51990d4fe2e
Austin R <150394141+stone-rose32@users.noreply.github.com>
2024-05-07 11:46:32 -0500

    Update home.py

--- home.py
+++ home.py
@@ -1,8 +1,15 @@
 import streamlit as st
 from pages import about, project_details, data_and_visuals, project_code
-
+#https://blog.streamlit.io/crafting-a-dashboard-app-in-python-using-streamlit/
 def show():
     st.title("Introduction")
+    col = st.columns((1.5, 4.5, 2), gap='medium')
+    with col[0]:
+        st.text("Columns 1 text")
+    with col[1]:
+        st.text("Columns 2 text")
+    with col[2]:
+        st.text("Columns 3 text")
 
 def main():
     show()
