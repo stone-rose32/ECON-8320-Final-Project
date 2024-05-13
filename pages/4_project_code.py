28a1343a4a7a114d4d63a8fa231573346dfe6078
Austin R <150394141+stone-rose32@users.noreply.github.com>
2024-05-12 22:12:32 -0500

    Finalize 4_project_code.py

--- pages/4_project_code.py
+++ pages/4_project_code.py
@@ -1,11 +1,13 @@
 import streamlit as st
 import requests
 
+# Create a function to get the data from the URL
 def get_data(url):
   response = requests.get(url)
   return response.text
 
 def main():
+    # Create a section for the project code
     st.title("Project Code")
     pc_intro_text = """
     This tab's purpose is to show the code used in this project. Ultimately, there are aspects
@@ -20,29 +22,31 @@ def main():
     """
     st.write(pc_intro_text)
     
-    tab1, tab2, tab3, tab4, tab5, tab6 = st.tabs(["Initial Data Extraction", "Streamlit Application Core", "About Page", "Data and Visual Page", "Project Details Page", "Project Code Page"])
+    # Create tabs for the different sections of the project code
+    tab1, tab2, tab3, tab4, tab5, tab6 = st.tabs(["Initial Data Extraction", "Streamlit Application Core", "About Page", "GetCensusData Class", "Data and Visual Page", "Project Code Page"])
+    # Load the code from the respective URLs
     with tab1:
         app_data_url = 'https://raw.githubusercontent.com/stone-rose32/ECON-8320-Final-Project/main/app_data.py'
         data_code = get_data(app_data_url)
         st.header('Initial Data Extraction Code')
         st.code(data_code, language='python')
     with tab2:
-        app_data_url = 'https://raw.githubusercontent.com/stone-rose32/ECON-8320-Final-Project/main/streamlit_app.py'
+        app_data_url = 'https://raw.githubusercontent.com/stone-rose32/ECON-8320-Final-Project/main/home.py'
         data_code = get_data(app_data_url)
         st.header('Streamlit Application Core Code')
         st.code(data_code, language='python')
     with tab3:
-        app_data_url = 'https://raw.githubusercontent.com/stone-rose32/ECON-8320-Final-Project/main/pages/about.py'
+        app_data_url = 'https://raw.githubusercontent.com/stone-rose32/ECON-8320-Final-Project/main/pages/1_about.py'
         data_code = get_data(app_data_url)
         st.header('About Page Code')
         st.code(data_code, language='python')
     with tab4:
-        app_data_url = 'https://raw.githubusercontent.com/stone-rose32/ECON-8320-Final-Project/main/pages/data_and_visuals.py'
+        app_data_url = 'https://raw.githubusercontent.com/stone-rose32/ECON-8320-Final-Project/main/GetCensusData.py'
         data_code = get_data(app_data_url)
-        st.header('Introdution Page Code')
+        st.header('GetCensusData Class Code')
         st.code(data_code, language='python')
     with tab5:
-        app_data_url = 'https://raw.githubusercontent.com/stone-rose32/ECON-8320-Final-Project/main/pages/project_details.py'
+        app_data_url = 'https://raw.githubusercontent.com/stone-rose32/ECON-8320-Final-Project/main/pages/3_data_and_visuals.py'
         data_code = get_data(app_data_url)
         st.header('Data and Visual Page Code')
         st.code(data_code, language='python')
@@ -52,5 +56,6 @@ def main():
         st.header('Project Code Page Code')
         st.code(data_code, language='python')
 
+# Run the main function
 if __name__ == "__main__":
     main()
