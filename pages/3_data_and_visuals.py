63b6e2b27655b58877e6e0de16ab0376c67d183d
Austin R <150394141+stone-rose32@users.noreply.github.com>
2024-05-13 18:22:03 -0500

    Update 3_data_and_visuals.py

--- pages/3_data_and_visuals.py
+++ pages/3_data_and_visuals.py
@@ -2,17 +2,12 @@ import streamlit as st
 import pandas as pd
 import plotly.express as px
 
-# Create a function to load the data into streamlit. @st.cache_data is used to cache the data so that it is not reloaded every time the page is refreshed.
 @st.cache_data
 def load_data(filename):
-    # Load the data in chunks to avoid memory issues
     chunk_size = 50_000
     chunks = []
-    # Read the data in chunks
     for chunk in pd.read_csv(filename, chunksize=chunk_size, low_memory=False):
         chunks.append(chunk)
-    
-    # Concatenate the chunks into a single dataframe
     data = pd.concat(chunks, axis=0)
     return data
 
@@ -22,13 +17,8 @@ def remove_non_numeric_rows(data, columns):
     return data
 
 def convert_columns_to_int(data):
-    # List of columns to convert to integers
-    columns_to_convert = [col for col in data.columns if 'weight' in col.lower() or 'year' in col.lower() or 'household total #' in col.lower()]
-    
-    # Remove rows with non-numeric values in the specified columns
+    columns_to_convert = [col for col in data.columns if 'weight' in col.lower() or 'year' in col.lower() or 'household-total #' in col.lower()]
     data = remove_non_numeric_rows(data, columns_to_convert)
-    
-    # Convert selected columns to integers
     for col in columns_to_convert:
         data[col] = pd.to_numeric(data[col], errors='coerce').fillna(0).astype(int)
     return data
@@ -36,17 +26,13 @@ def convert_columns_to_int(data):
 def show():
     st.title("Data and Visuals")
 
-    # Load the data
     data = load_data("https://github.com/stone-rose32/ECON-8320-Final-Project/releases/download/v1.0.0/census_data.csv")
     data = convert_columns_to_int(data)
     
-    # Create tabs for the different sections of the data and visuals
     tab1, tab2 = st.tabs(["Visualizations", "Data Table"])
     with tab1:
         st.write('CPS Data Analysis')
-        # Create a sidebar to allow users to filter the data. Removed Weight fields from dropbox
         filtered_columns = data.filter(regex='^(?!.*Weight).*$', axis=1)
-        # Create a sidebar to allow users to filter the data
         with st.sidebar:
             cities = ['Select All'] + data['Metropolitan Core Based Statistical Area FIPS Code'].unique().tolist()
             select_loc = st.selectbox('Select a location:', cities)
@@ -57,13 +43,11 @@ def show():
             var_values = ['Select All'] + data[selected_variable].unique().tolist()
             selected_var_value = st.selectbox('Filter by variable values:', var_values)
         
-        # Filter the data based on the selected location and year range
         if select_loc == 'Select All':
             filtered_data = data[(data['Year'].between(year_range[0], year_range[1]))]
         else:
             filtered_data = data[(data['Year'].between(year_range[0], year_range[1])) & (data['Metropolitan Core Based Statistical Area FIPS Code'] == select_loc)]
         
-        # Filter the data based on the selected own/rent and variable values
         if own_rent_selected != 'Select All':
             filtered_data = filtered_data[filtered_data['Household-own/rent living quarters'] == own_rent_selected]
         
@@ -72,7 +56,6 @@ def show():
         
         fig = None
 
-        # Show the filtered data in various charts
         if selected_variable == 'Metropolitan Core Based Statistical Area FIPS Code':        
             own_living_quarters = filtered_data[filtered_data['Household-own/rent living quarters'] == 'Owned Or Being Bought By A Hh Member']
             own_quarters_by_area = own_living_quarters.groupby('Metropolitan Core Based Statistical Area FIPS Code')['Weight-composited final weight'].sum().reset_index()
@@ -112,19 +95,15 @@ def show():
         elif selected_variable == 'Year' or selected_variable == 'Month':
             st.write('This variable is not suitable for visualization. Please select another variable.')
     
-        # If a figure was created, show it
         if fig:
             st.plotly_chart(fig)
         else:
             st.write("")
-    # Show the data table
     with tab2:    
         st.dataframe(filtered_data)
 
-# Create a main function to run the data and visuals page
 def main():
     show()
 
-# Run the main function
 if __name__ == "__main__":
     main()
